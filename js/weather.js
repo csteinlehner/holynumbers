1ca80a361c8d5913cce06f2ac5a179b03e27f43e
csteinlehner <christoph@csteinlehner.com>
2018-01-26 20:14:23 +0200

    Weather now drawn as min/max bar whole year

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,9 @@
 var margin = {top: 40, right: 40, bottom: 40, left: 40};
 
-var visWidth = 1080 - margin.left - margin.right;
-var visHeight = 1080 - margin.top - margin.bottom;
 
-var drawWeather = function (requestedYear) {
+var drawWeather = function (requestedYear, countryFile) {
+    var visWidth = 1400 - margin.left - margin.right;
+    var visHeight = 1080 - margin.top - margin.bottom;
     
 
     var svg = d3.select("svg")
@@ -16,13 +16,13 @@ var drawWeather = function (requestedYear) {
 
     var x = d3.scaleLinear()
             .rangeRound([0, visWidth])
-            .domain([0, 31]);
+            .domain([0, 365]);
     var y = d3.scaleLinear()
             .rangeRound([0, visHeight])
             .domain([400, -200]);
 
 
-    d3.json("data/GME00111445.json", function(error, wData) {
+    d3.json("data/"+countryFile, function(error, wData) {
         if (error) return console.error(error);
 
         wData.years;
@@ -37,42 +37,48 @@ var drawWeather = function (requestedYear) {
         .attr("class", "axis axis--y")
         .call(d3.axisLeft(y));
 
-        console.log(d3.max(yearData.months[0].days, function(d) { return d.values.TMAX; }))
-        g.selectAll(".bar")
-            .data(yearData.months[0].days)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", function(d) { return x(parseInt(d.key)); })
-            // .attr("y", function(d) { return y(parseInt(d.values.TMAX)); })
-            .attr("y", function(d) { return y(parseInt(d.values.TMAX)); })
-            .attr("width", 10)
-            .attr("height", function(d) { return y(parseInt(d.values.TMIN)) - y(parseInt(d.values.TMAX)); });
+        g.selectAll(".month")
+            .data(yearData.months)
+            .enter().append('g')
+            .each(drawMonth)
 
-        g.selectAll(".bar")
-            .data(yearData.months[0].days)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", function(d) { return x(parseInt(d.key)); })
-            // .attr("y", function(d) { return y(parseInt(d.values.TMAX)); })
-            .attr("y", function(d) { return y(parseInt(d.values.TMAX)); })
-            .attr("width", 10)
-            .attr("height", function(d) { return y(parseInt(d.values.TMIN)) - y(parseInt(d.values.TMAX)); });
+        function drawMonth(d, i) {
+                var m = parseInt(d.key);
+                 
+                d3.select(this).selectAll(".bar")
+                .data(d.days)
+                .enter()
+                    .append("rect")
+                    .attr("class", "bar")
+                    .attr("x", function(dd) { 
+                        var yDay = parseInt(dd.key);
+                        // console.log(dd.key + '/'+ String(m) + ' : ' + daysIntoYear(new Date(requestedYear, m-1, yDay)));
+                        return x(daysIntoYear(new Date(requestedYear, m-1, yDay))); 
+                    })
+                    .attr("y", function(dd) { return y(parseInt(dd.values.TMAX)); })
+                    // .attr(" y", function(dd) { return y(parseInt(dd.key)); })
+                    .attr("width", 3)
+                    .attr("height", function(dd) { return y(parseInt(dd.values.TMIN)) - y(parseInt(dd.values.TMAX)); });
+            }
 
-        g.selectAll(".labelTMAX")
-            .data(yearData.months[0].days)
-            .enter().append("text")
-            .attr("x", function(d) { return x(parseInt(d.key)); })
-            .attr("y", function(d) { return y(parseInt(d.values.TMAX)+20); })
-            .attr("dy", ".35em")
-            .text(function(d) { return parseFloat(d.values.TMAX); });
+        function daysIntoYear(date){
+            return (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
+        }
+        // g.selectAll(".labelTMAX")
+        //     .data(yearData.months[0].days)
+        //     .enter().append("text")
+        //     .attr("x", function(d) { return x(parseInt(d.key)); })
+        //     .attr("y", function(d) { return y(parseInt(d.values.TMAX)+5); })
+        //     .attr("dy", ".35em")
+        //     .text(function(d) { return parseFloat(d.values.TMAX); });
         
-        g.selectAll(".labelTMIN")
-            .data(yearData.months[0].days)
-            .enter().append("text")
-            .attr("x", function(d) { return x(parseInt(d.key)); })
-            .attr("y", function(d) { return y(parseInt(d.values.TMIN)-20); })
-            .attr("dy", ".35em")
-            .text(function(d) { return parseFloat(d.values.TMIN); });
+        // g.selectAll(".labelTMIN")
+        //     .data(yearData.months[0].days)
+        //     .enter().append("text")
+        //     .attr("x", function(d) { return x(parseInt(d.key)); })
+        //     .attr("y", function(d) { return y(parseInt(d.values.TMIN)-5); })
+        //     .attr("dy", ".35em")
+        //     .text(function(d) { return parseFloat(d.values.TMIN); });
 
 
         // yearData.months[0].days[0].values.TMAX
